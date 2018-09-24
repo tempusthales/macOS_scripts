ed8e9f4b1aa5683ae2a78db4107c8989ac2e9cc3
Gilbert Palau <gilbertpalau@gmail.com>
2018-09-23 19:31:11 -0500

    Initial Commit
    Script for injecting a new admin account in macOS. Should work with Sierra, High Sierra and Mojave.

---
+++ create_admin_account_script/add_new_admin.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Script for injecting a new admin account into macOS 
+# if an account with the same name is used, this script will not work.
+
+adminaccount="youradminhere"
+
+dscl . -create /Users/$adminaccount
+dscl . -create /Users/$adminaccount RealName "Your Admin"
+dscl . -passwd /Users/$adminaccount "500+Skokos+301"
+dscl . -create /Users/$adminaccount UniqueID 501
+dscl . -create /Users/$adminaccount PrimaryGroupID 80
+dscl . -create /Users/$adminaccount UserShell /bin/bash
+dscl . -create /Users/$adminaccount NFSHomeDirectory /Users/$adminaccount
+scp -R /System/Library/User\ Template/English.lproj /Users/$adminaccount
+chown -R $adminaccount:staff /Users/$adminaccount
+
+dseditgroup -o edit -d admin -t group com.apple.access_ssh
+dscl . append /Groups/com.apple.access_ssh user $adminaccount
+dscl . append /Groups/com.apple.access_ssh GroupMembership $adminaccount
+dscl . append /Groups/com.apple.access_ssh groupmembers `dscl . read /Users/$adminaccount GeneratedUID | cut -d " " -f 2`
+
+# Setting up ARD for use of new account
+systemsetup -setremotelogin on
+System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -specifiedUsers
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -users $adminaccount -access -on -privs -DeleteFiles -ControlObserve -TextMessages -OpenQuitApps -GenerateReports -RestartShutDown -SendFiles -ChangeSettings
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -activate -restart -console
+
+exit 0
