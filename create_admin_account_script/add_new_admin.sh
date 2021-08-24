b8fe4609a7a1b20176f0f4e20c73feab3af55730
Gilbert Palau <gilbertpalau@gmail.com>
2021-08-24 10:54:05 -0500

    Update add_new_admin.sh

--- create_admin_account_script/add_new_admin.sh
+++ create_admin_account_script/add_new_admin.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+f#!/bin/sh
 
 # Script for injecting a new admin account into macOS 
 # if an account with the same name is used, this script will not work.
@@ -7,7 +7,7 @@ adminaccount="youradminhere"
 
 dscl . -create /Users/$adminaccount
 dscl . -create /Users/$adminaccount RealName "Your Admin"
-dscl . -passwd /Users/$adminaccount "500+Skokos+301"
+dscl . -passwd /Users/$adminaccount "dumbpassword"
 dscl . -create /Users/$adminaccount UniqueID 501
 dscl . -create /Users/$adminaccount PrimaryGroupID 80
 dscl . -create /Users/$adminaccount UserShell /bin/bash
