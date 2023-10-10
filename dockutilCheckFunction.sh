bfec7d1ad7132705348c5b46cb87260a503d2a67
Gilbert Palau <gilbertpalau@gmail.com>
2023-10-10 12:56:47 -0500

    Update dockutilCheckFunction.sh
    Updated verification method...

--- dockutilCheckFunction.sh
+++ dockutilCheckFunction.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
 
-# The work here in this function is heavily influenced from Adam Codega's dialogCheck function - https://github.com/acodega/dialog-scripts/blob/main/dialogCheckFunction.sh
-# This function is Bash compatible, insert in your script and then place dockutilCheck where you want it to be executed
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# DESC:  dockutilCheck function
+#
+# Developed by: Tempus Thales ~ Austin, TX
+# Thanks to Adam Codega and Armin Briegel for knowledge used in this function.
+# Adam Codega - https://github.com/acodega and Armin Briegel - https://github.com/scriptingosx
+# Start Date: 10/09/2023        End Date: 10/10/2023
+#
+# Instructions: put this somewhere in your code then call the function later on by typinig dockutilCheck somewhere else
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 function dockutilCheck (){
     # Get the URL of the latest PKG from the DockUtil GitHub repo
     dockutilURL=$(curl --silent --fail "https://api.github.com/repos/kcrawford/dockutil/releases/latest" | awk -F '"' "/browser_download_url/ && /pkg\"/ { print \$4; exit }")
-    
-    # Expected Team ID of the downloaded PKG
-    # expectedDialogTeamID="TeamIDGoesHere"
+    expectedDialogTeamID="Z5J8CJBUWC" # This is the DEVELOPER ID, not the github TeamID as I thought it was... I was an idiot.
 
     # Check for DockUtil and install if not found
     if [ ! -e "/usr/local/bin/dockutil" ]; then
         echo "DockUtil not found. Installing..."
         # Create temporary working directory
-        workDirectory=$( /usr/bin/basename "$0" )
-        tempDirectory=$( /usr/bin/mktemp -d "/private/tmp/$workDirectory.XXXXXX" )
-        #Download the installer package
-        /usr/bin/curl --location --silent "$dockutilURL" -o "$tempDirectory/DockUtil.pkg"
+        workDirectory=$(basename "$0")
+        tempDirectory=$(mktemp -d "/private/tmp/$workDirectory.XXXXXX")
+        
+        # Download the installer package
+        if ! /usr/bin/curl --location --silent "$dockutilURL" -o "$tempDirectory/DockUtil.pkg"; then
+            echo "Failed to download DockUtil package. Exiting."
+            exit 1
+        fi
+
         # Verify the download
-        teamID=$(/usr/sbin/spctl -a -vv -t install "$tempDirectory/DockUtil.pkg" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
+        teamID=$(/usr/sbin/spctl -a -t install -vv "$tempDirectory/DockUtil.pkg" 2>&1 | awk '/origin=/ {print $NF }' | tr -d '()')
+        
         # Install the package if Team ID validates
-        # if [ "$expectedDialogTeamID" = "$teamID" ] || [ "$expectedDialogTeamID" = "" ]; then
-        #   /usr/sbin/installer -pkg "$tempDirectory/DockUtil.pkg" -target /
+        if [ "$expectedDialogTeamID" = "$teamID" ] || [ "$expectedDialogTeamID" = "" ]; then
+           /usr/sbin/installer -pkg "$tempDirectory/DockUtil.pkg" -target /
+        
+        # Install the package if Team ID validates
+        if [ -z "$teamID" ]; then
+            echo "Team ID verification failed. Exiting."
+            exit 1
+        fi
+
         /usr/sbin/installer -pkg "$tempDirectory/DockUtil.pkg" -target /
-        # else # uncomment this else if you want your script to exit now if swiftDialog is not installed
-            # displayAppleScript # uncomment this if you're using my displayAppleScript function
-            # echo "Dialog Team ID verification failed."
-            # exit 1
-    fi
+    # else
+        echo "DockUtil found. Proceeding..."
+    # fi
+
     # Remove the temporary working directory when done
     /bin/rm -Rf "$tempDirectory"  
-  else echo "DockUtil found. Proceeding..."
-  fi
 }
