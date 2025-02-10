ae5e683890a2ad4b9cd128c12aa4d7af974c9c41
Gilbert Palau <gilbertpalau@gmail.com>
2025-02-10 17:13:46 -0600

    Update disable_sharing.sh
    update logMessage function

--- macOS_cpe_scripts/Mojave/disable_sharing.sh
+++ macOS_cpe_scripts/Mojave/disable_sharing.sh
@@ -9,19 +9,23 @@ log_path="/var/log/company_name"
 # log_file=""
 log_file="com.company_name.macos.preferences.log"
 
-logMessage () {
+logMessage() {
+  # Ensure log_path is set
+  [ -z "$log_path" ] && log_path="/var/log/default"
 
-  mkdir -p $log_path
+  mkdir -p "$log_path"
 
-  date_set="$((date +%Y-%m-%d..%H:%M:%S-%z) 2>&1)"
-  user="$((who -m | awk '{print $1;}') 2>&1)"
-  if [[ "$log_file" == "" ]]; then
-    # write to stdout (capture by Jamf script logging)
+  # Correct command substitution
+  date_set="$(date +%Y-%m-%d..%H:%M:%S-%z 2>&1)"
+  user="$(who -m | awk '{print $1;}' 2>&1)"
+
+  if [[ -z "$log_file" ]]; then
+    # Write to stdout (captured by Jamf script logging)
     echo "$date_set    $user    ${0##*/}    $1"
   else
-    # write local logs
-    echo "$date_set    $user    ${0##*/}    $1" >> $log_path/$log_file
-    # write to stdout (capture by Jamf script logging)
+    # Write to local logs
+    echo "$date_set    $user    ${0##*/}    $1" >> "$log_path/$log_file"
+    # Also write to stdout
     echo "$date_set    $user    ${0##*/}    $1"
   fi
 }
