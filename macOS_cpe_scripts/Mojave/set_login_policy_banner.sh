2a55d637b651fde09ee1c565b5e9b0a4bf6e8e89
Gilbert Palau <gilbertpalau@gmail.com>
2025-02-10 17:28:58 -0600

    Update set_login_policy_banner.sh

--- macOS_cpe_scripts/Mojave/set_login_policy_banner.sh
+++ macOS_cpe_scripts/Mojave/set_login_policy_banner.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ####################################################################################
 # Name:                       set_login_policy_banner.sh
+# Author:                      Tempus Thales
 # Purpose:                    Sets login policy banner
 ####################################################################################
 # Establish standardized local logging logic
@@ -10,22 +11,27 @@ log_path="/var/log/company_name"
 # log_file=""
 log_file="com.company_name.macos.security.log"
 
-logMessage () {
+logMessage() {
+  # Ensure log_path is set
+  [ -z "$log_path" ] && log_path="/var/log/default"
 
-  mkdir -p $log_path
+  mkdir -p "$log_path"
 
-  date_set="$((date +%Y-%m-%d..%H:%M:%S-%z) 2>&1)"
-  user="$((who -m | awk '{print $1;}') 2>&1)"
-  if [[ "$log_file" == "" ]]; then
-    # write to stdout (capture by Jamf script logging)
+  # Correct command substitution
+  date_set="$(date +%Y-%m-%d..%H:%M:%S-%z 2>&1)"
+  user="$(who -m | awk '{print $1;}' 2>&1)"
+
+  if [[ -z "$log_file" ]]; then
+    # Write to stdout (captured by Jamf script logging)
     echo "$date_set    $user    ${0##*/}    $1"
   else
-    # write local logs
-    echo "$date_set    $user    ${0##*/}    $1" >> $log_path/$log_file
-    # write to stdout (capture by Jamf script logging)
+    # Write to local logs
+    echo "$date_set    $user    ${0##*/}    $1" >> "$log_path/$log_file"
+    # Also write to stdout
     echo "$date_set    $user    ${0##*/}    $1"
   fi
 }
+
 #####################################################################################
 # Write /private/etc/sudoers.d/defaults_timestamp_timeout
 #####################################################################################
